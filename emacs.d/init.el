16efbd06a41146ea2b18951f2e22171b6ebc789a
Dr. Yad Tahir <yad@ieee.org>
2019-05-20 15:08:25 +0300

    Update init.el in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,19 +3,19 @@
 (do-modules
 	"core/evil"
 	"ui/general"
-	"ui/theme" ;; don't put it on top; this increases the startup time, somehow!
-	"ui/status-bar" ;;slow - 100ms
+	"ui/theme" ;; don't put it on top; it increases the startup time somehow!
+	"ui/status-bar" ;; slow
 	"ui/windows"
 	"ui/buffers"
 	"ui/line-numbering"
 	"ui/text-visual"
 
-	"completion/ivy" ;;slow
+	"completion/ivy" ;; slow
 	"completion/company"
 	"completion/commands" ;; a bit slow
-	"completion/snippets" ;; key bindings
+	"completion/snippets"
 
-	"feature/git" ;;move it to tool; ohh dear!
+	"feature/git" ;; move it to tool - Ohh dear!
 	"feature/scratches"
 	"feature/text-adjust"
 	"feature/spell-checker"
@@ -31,14 +31,14 @@
 	"lang/elisp"
 	"lang/go"
 	"lang/clojure"
-	;; ;; "lang/java"
-	;; ;; "lang/web"
-	;; ;; "lang/php"
+	;; "lang/java"
+	;; "lang/web"
+	;; "lang/php"
 
 	"tool/notebook"
-	"tool/music" ;; optimization
+	"tool/music" ;; needs optimization
 	"tool/english"
-	"tool/shell" ;; cleanning
+	"tool/shell" ;; cleaning
 	;; ;; "tool/pdf"
 	"tool/calculator"
 	;; ;; "tool/dashboard"
@@ -46,28 +46,3 @@
 (do-modules-load t)
 ;; (do-modules-bootstrap-load)
 (put 'dired-find-alternate-file 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-format-latex-options
-   (list :foreground chocolate-theme-white :background chocolate-theme-bg :scale 1.0 :html-foreground chocolate-theme-white :html-background "Transparent" :html-scale 1.0))
- '(package-selected-packages
-   (quote
-	(eterm-256color fish-completion define-word simple-mpc flycheck-clojure cider clojure-mode company-go go-dlv go-mode macrostep flycheck org-bullets exec-path-from-shell vlf openwith dired-subtree dired-collapse dired-open s drag-stuff evil-lion evil-commentary expand-region smartparens evil-surround magit auto-yasnippet yasnippet which-key company-quickhelp company-statistics company-flx company wgrep counsel-projectile counsel projectile ivy highlight-parentheses rainbow-mode evil-anzu telephone-line evil general use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(term-color-black ((t (:foreground "#654d4d" :background nil))))
- '(term-color-blue ((t (:foreground "#6fd8ff"))))
- '(term-color-cyan ((t (:foreground "#6fd8ff"))))
- '(term-color-green ((t (:foreground "#a7e23e"))))
- '(term-color-magenta ((t (:foreground "#6fd8ff"))))
- '(term-color-red ((t (:foreground "#ff4422"))))
- '(term-color-white ((t (:foreground "#e6beae"))))
- '(term-color-yellow ((t (:foreground "#ffa500"))))
- '(term-default-bg-color ((t (:inherit term-color-black))))
- '(term-default-fg-color ((t (:inherit term-color-white)))))
