74925c3464f356ed90efd4e14b1852dbb2cbe9c9
Dr. Yad Tahir <yad@ieee.org>
2019-05-20 15:08:41 +0300

    Add keyboard layout toggling

--- lemon-bar/.config/lemon-bar/blocks/keyboard.sh
+++ lemon-bar/.config/lemon-bar/blocks/keyboard.sh
@@ -2,7 +2,8 @@
 #
 
 while true; do
-	state=$(setxkbmap -query | awk '/^layout/{split($2,arr,","); print toupper(arr[1])}')
+	state=$(setxkbmap -query |
+				awk '/^layout/{split($2,arr,","); print toupper(arr[1])}')
 	if [ $state = "DEVORK" ]; then
 		echo "Sk%{F${COLOR_INDICATOR1}}$state%{F-}"
 	else
---
+++ scripts/bin/toggle-keyboard-layout.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. /home/yad/bin/settings.sh
+
+state=$(setxkbmap -query |
+				awk '/^layout/{split($2,arr,","); print toupper(arr[1])}')
+
+if [ $state = "DVORAK" ]; then
+	setxkbmap us
+	setxkbmap -option
+else
+	setxkbmap dvorak
+	# setxkbmap -option altwin:swap_alt_win
+fi
+
+~/.config/lemon-bar/blocks/keyboard.sh > $PANEL_FIFO
