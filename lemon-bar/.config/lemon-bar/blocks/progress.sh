bc606ef52f520fbdb64cb63df965f1440e2a55d4
Dr. Yad Tahir <yad@ieee.org>
2019-05-20 22:50:25 +0300

    Modify the lemon bar to monitor the load progress in Firefox tabs.
    This commit is to incorporate my new side project: https://github.com/yad-tahir/firefox-progress-title.

--- lemon-bar/.config/lemon-bar/blocks/progress.sh
+++ lemon-bar/.config/lemon-bar/blocks/progress.sh
@@ -19,43 +19,24 @@
 # 02110-1301, USA.
 
 # A utility script to draw a progress using text underline.
+
 text="$1"
 progress="$2"
 
-# Get text length
-len="${#text}"
-
-# Calculate the number of characters that needs to be underline
 if [ $progress -eq $progress 2> /dev/null ]; then
-	underline_num=$((len*progress/100))
+	len="${#text}"
+	# Calculate the number of characters that needs to be underline
+	underline_num=$(($len*progress/100))
 
-	# Make sure $underline_num never exceed the text length
+	# Make sure $underline_num never exceeds the length
 	if [ "$underline_num" -gt $len ]; then
 		underline_num=$len
 	fi
 
-	# Create the output
-	output="%{+u}"
-	chars="$text"
-	i=0
-	# Loop to go through underlined chars
-	len=$(($len+1))
-	while [ $i -lt $len ]; do
-		# Get the first char from the character set
-		first="${chars%$text}"
-		# Append
-		output="${output}${first}"
-		# Update the character set
-		chars=$text
-		# Remove the first char char
-		text="${text#?}"
-		# Stop the underline when we reach the character at $underline_num
-		if [ $i = $underline_num ]; then
-			output="${output}%{-u}"
-		fi
+	output=$(echo "$text" |
+				 awk -v UNDER="$underline_num" \
+					 '{print "%{+u}"substr($0,0,UNDER) \
+				"%{-u}"substr($0,UNDER+1,length($0))}')
 
-		i=$((i+1))
-	done
-
-	echo $output
+	echo "$output"
 fi
