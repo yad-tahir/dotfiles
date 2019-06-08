993b99b4f475069321aadc842bc6599761cb8dc7
Dr. Yad Tahir <yad@ieee.org>
2019-06-08 20:45:28 +0300

    Improve Emacs' spelling mode

--- emacs.d/modules/feature/spell-checker.el
+++ emacs.d/modules/feature/spell-checker.el
@@ -20,4 +20,30 @@
 (use-package flyspell
   :ensure t
   :hook ((text-mode . flyspell-mode)
-	 (prog-mode . flyspell-prog-mode)))
+		 (prog-mode . flyspell-prog-mode))
+  :config
+  (general-define-key
+   :keymaps 'flyspell-mode-map
+   "M-]" 'evil-next-flyspell-error
+   "M-[" 'evil-prev-flyspell-error)
+
+  (general-define-key
+   :keymaps 'flyspell-mouse-map
+   "C-<return>" 'ispell-word
+   "M-<return>" 'ispell-word)
+
+  (setq ispell-personal-dictionary "~/notes/personal.aspell.en.pws"
+		flyspell-issue-message-flag nil
+		flyspell-delay 5
+		ispell-help-in-bufferp nil)
+
+  (add-hook 'text-mode-hook 'flyspell-buffer)
+  (add-hook 'prog-mode-hook 'flyspell-buffer))
+
+(use-package flyspell-correct
+  :ensure t
+  :commands 'flyspell-correct-wrapper
+  :init
+  (general-define-key
+   :keymaps 'flyspell-mouse-map
+   "C-<return>" 'flyspell-correct-at-point))
