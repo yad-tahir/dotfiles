fe981a78d325bd9b6ebaff2603cda5e0c1a806df
Dr. Yad Tahir <yad@ieee.org>
2019-06-19 08:08:53 +0300

    Simplify text surrounding operations in Emacs
    This commit removes some redundant keybindings.

--- emacs.d/modules/feature/text-adjust.el
+++ emacs.d/modules/feature/text-adjust.el
@@ -27,216 +27,193 @@
    "xj" 'fusion-join
    "xJ" 'fusion-split))
 
-  (use-package evil-surround
-	:ensure t
-	:defer 10
-	:init
-	;; Eliminate extra space between pairs
-	(setq evil-surround-pairs-alist
-		  '((?\( . ("(" . ")"))
-			(?\[ . ("[" . "]"))
-			(?\{ . ("{" . "}"))
+(use-package evil-surround
+  :ensure t
+  :commands (evil-surround-edit
+			 evil-surround-change
+			 evil-surround-region
+			 evil-surround-delete)
+  :init
+  (general-define-key
+   :states 'normal
+   "s" '(:ignore t :which-key "surround")
+   "ss" 'evil-surround-edit
+   "su" 'evil-surround-change
+   "sd" 'evil-surround-delete)
 
-			(?\) . ("(" . ")"))
-			(?\] . ("[" . "]"))
-			(?\} . ("{" . "}"))
+  (general-define-key
+   :states 'visual
+   "s" '(:ignore t :which-key "surround")
+   "ss" 'evil-surround-region
+   "su" 'evil-surround-change
+   "sd" 'evil-surround-delete)
+  :config
+  ;; Eliminate extra space between pairs
+  (setq evil-surround-pairs-alist
+		'((?\( . ("(" . ")"))
+		  (?\[ . ("[" . "]"))
+		  (?\{ . ("{" . "}"))
 
-			(?# . ("#{" . "}"))
-			(?b . ("(" . ")"))
-			(?B . ("{" . "}"))
-			(?> . ("<" . ">"))
-			(?t . evil-surround-read-tag)
-			(?< . evil-surround-read-tag)
-			(?f . evil-surround-function)))
-	:config
-	(general-define-key
-	 :states '(normal)
-	 :prefix "x"
-	 "s" '(:ignore t :which-key "surround")
-	 "ss" 'evil-surround-edit
-	 "si" 'evil-surround-edit
-	 "sa" 'evil-surround-edit
-	 "su" 'evil-surround-change
-	 "sd" 'evil-surround-delete)
+		  (?\) . ("(" . ")"))
+		  (?\] . ("[" . "]"))
+		  (?\} . ("{" . "}"))
 
-	(general-define-key
-	 :states '(visual)
-	 :prefix "x"
-	 "s" '(:ignore t :which-key "surround")
-	 "ss" 'evil-surround-region
-	 "si" 'evil-surround-region
-	 "sa" 'evil-surround-region
-	 "su" 'evil-surround-change
-	 "sd" 'evil-surround-delete)
+		  (?# . ("#{" . "}"))
+		  (?b . ("(" . ")"))
+		  (?B . ("{" . "}"))
+		  (?> . ("<" . ">"))
+		  (?t . evil-surround-read-tag)
+		  (?< . evil-surround-read-tag)
+		  (?f . evil-surround-function))))
 
-	(general-define-key
-	 :states '(normal)
-	 ;; :prefix "g"
-	 "s" '(:ignore t :which-key "surround")
-	 "ss" 'evil-surround-edit
-	 "si" 'evil-surround-edit
-	 "sa" 'evil-surround-edit
-	 "su" 'evil-surround-change
-	 "sd" 'evil-surround-delete)
+(use-package smartparens
+  :ensure t
+  :hook ((prog-mode . turn-on-smartparens-mode))
+  :config
+  (require 'smartparens-config)
+  (setq sp-highlight-pair-overlay nil
+		sp-highlight-wrap-overlay nil
+		sp-highlight-wrap-tag-overlay nil)
 
-	(general-define-key
-	 :states '(visual)
-	 ;; :prefix "g"
-	 "s" '(:ignore t :which-key "surround")
-	 "ss" 'evil-surround-region
-	 "si" 'evil-surround-region
-	 "sa" 'evil-surround-region
-	 "su" 'evil-surround-change
-	 "sd" 'evil-surround-delete))
+  (general-define-key
+   :keymaps 'smartparens-mode-map
+   :states 'normal
+   "M-n" 'sp-forward-slurp-sexp
+   "M-h" 'sp-forward-barf-sexp
+   "g M-n" 'sp-backward-slurp-sexp
+   "g M-h" 'sp-backward-barf-sexp
+   "xs" '(:ignore t :which-key "smart parens")
+   "xsn" 'sp-forward-slurp-sexp
+   "xsN" 'sp-forward-barf-sexp
+   "xsh" 'sp-backward-slurp-sexp
+   "xsH" 'sp-backward-barf-sexp
+   "xsc" 'sp-up-sexp
+   "xst" 'sp-down-sexp
+   "xsu" 'sp-splice-sexp
+   "xsj" 'sp-join-sexp))
 
-  (use-package smartparens
-	:ensure t
-	:hook ((prog-mode . turn-on-smartparens-mode))
-	:config
-	(require 'smartparens-config)
-	(setq sp-highlight-pair-overlay nil
-		  sp-highlight-wrap-overlay nil
-		  sp-highlight-wrap-tag-overlay nil)
+;; (use-package evil-paredit
+;; 	:disabled t
+;; 	:ensure t
+;; 	:hook ((prog-mode . evil-paredit-mode)
+;; 		   (prog-mode . paredit-mode))
+;; 	:config
+;; 	;; Reset keymaps
+;; 	(setf (cdr evil-paredit-mode-map) nil)
 
-	(general-define-key
-	 :keymaps 'smartparens-mode-map
-	 :states 'normal
-	 "M-n" 'sp-forward-slurp-sexp
-	 "M-h" 'sp-forward-barf-sexp
-	 "g M-n" 'sp-backward-slurp-sexp
-	 "g M-h" 'sp-backward-barf-sexp
-	 "xs" '(:ignore t :which-key "smart parens")
-	 "xsn" 'sp-forward-slurp-sexp
-	 "xsN" 'sp-forward-barf-sexp
-	 "xsh" 'sp-backward-slurp-sexp
-	 "xsH" 'sp-backward-barf-sexp
-	 "xsc" 'sp-up-sexp
-	 "xst" 'sp-down-sexp
-	 "xsu" 'sp-splice-sexp
-	 "xsj" 'sp-join-sexp))
+;; 	(general-define-key
+;; 	 :keymaps 'evil-paredit-mode-map
+;; 	 :states 'normal
+;; 	 ;; Bindings for basic movements
+;; 	 "xsn" 'paredit-forward-slurp-sexp
+;; 	 "xsN" 'paredit-forward-barf-sexp
+;; 	 "xsh" 'paredit-backward-slurp-sexp
+;; 	 "xsh" 'paredit-backward-barf-sexp
+;; 	 "M-n" 'paredit-forward-slurp-sexp
+;; 	 "M-h" 'paredit-forward-barf-sexp
+;; 	 "g M-h" 'paredit-backward-slurp-sexp
+;; 	 "g M-H" 'paredit-backward-barf-sexp
+;; 	 ;; Needed because we use 'u' instead of 'c'
+;; 	 "c" nil
+;; 	 "C" nil
+;; 	 "u" 'evil-paredit-change
+;; 	 "U" 'evil-paredit-change-line
+;; 	 "d" 'evil-paredit-delete
+;; 	 "D" 'evil-paredit-delete-line
+;; 	 "xsu" 'evil-change
+;; 	 "xsU" 'evil-change-line
+;; 	 "xsd" 'evil-delete
+;; 	 "xsD" 'evil-delete-line)
 
-  ;; (use-package evil-paredit
-  ;; 	:disabled t
-  ;; 	:ensure t
-  ;; 	:hook ((prog-mode . evil-paredit-mode)
-  ;; 		   (prog-mode . paredit-mode))
-  ;; 	:config
-  ;; 	;; Reset keymaps
-  ;; 	(setf (cdr evil-paredit-mode-map) nil)
+;; 	(general-define-key
+;; 	 :keymaps 'evil-paredit-mode-map
+;; 	 :states 'visual
+;; 	 ;; Surrounding region
+;; 	 "xs(" 'paredit-wrap-round
+;; 	 "xs)" 'paredit-wrap-round
+;; 	 "xs]" 'paredit-wrap-square
+;; 	 "xs[" 'paredit-wrap-square
+;; 	 "xs{" 'paredit-wrap-curly
+;; 	 "xs}" 'paredit-wrap-curly
+;; 	 "xs>" 'paredit-wrap-angled
+;; 	 "xs<" 'paredit-wrap-angled))
 
-  ;; 	(general-define-key
-  ;; 	 :keymaps 'evil-paredit-mode-map
-  ;; 	 :states 'normal
-  ;; 	 ;; Bindings for basic movements
-  ;; 	 "xsn" 'paredit-forward-slurp-sexp
-  ;; 	 "xsN" 'paredit-forward-barf-sexp
-  ;; 	 "xsh" 'paredit-backward-slurp-sexp
-  ;; 	 "xsh" 'paredit-backward-barf-sexp
-  ;; 	 "M-n" 'paredit-forward-slurp-sexp
-  ;; 	 "M-h" 'paredit-forward-barf-sexp
-  ;; 	 "g M-h" 'paredit-backward-slurp-sexp
-  ;; 	 "g M-H" 'paredit-backward-barf-sexp
-  ;; 	 ;; Needed because we use 'u' instead of 'c'
-  ;; 	 "c" nil
-  ;; 	 "C" nil
-  ;; 	 "u" 'evil-paredit-change
-  ;; 	 "U" 'evil-paredit-change-line
-  ;; 	 "d" 'evil-paredit-delete
-  ;; 	 "D" 'evil-paredit-delete-line
-  ;; 	 "xsu" 'evil-change
-  ;; 	 "xsU" 'evil-change-line
-  ;; 	 "xsd" 'evil-delete
-  ;; 	 "xsD" 'evil-delete-line)
+(use-package expand-region
+  :ensure t
+  :commands (er/expand-region)
+  :init
+  (general-define-key
+   :states 'visual
+   "r" 'er/expand-region))
 
-  ;; 	(general-define-key
-  ;; 	 :keymaps 'evil-paredit-mode-map
-  ;; 	 :states 'visual
-  ;; 	 ;; Surrounding region
-  ;; 	 "xs(" 'paredit-wrap-round
-  ;; 	 "xs)" 'paredit-wrap-round
-  ;; 	 "xs]" 'paredit-wrap-square
-  ;; 	 "xs[" 'paredit-wrap-square
-  ;; 	 "xs{" 'paredit-wrap-curly
-  ;; 	 "xs}" 'paredit-wrap-curly
-  ;; 	 "xs>" 'paredit-wrap-angled
-  ;; 	 "xs<" 'paredit-wrap-angled))
+(use-package evil-nerd-commenter
+  :ensure t
+  :commands (evilnc-comment-operator)
+  :init
+  (general-define-key
+   :states '(normal visual)
+   "g/" 'evilnc-comment-operator))
 
-  (use-package expand-region
-	:ensure t
-	:commands (er/expand-region)
-	:init
-	(general-define-key
-	 :states '(visual)
-	 "r" 'er/expand-region))
+;; As of June 14, evil-commentary is a bit buggy when the whitespace mode is on.
+;; (use-package evil-commentary
+;; 	:ensure t
+;; 	:commands (evil-commentary)
+;; 	:init
+;; 	(general-define-key
+;; 	 :states '(normal visual)
+;; 	 "g/" 'evil-commentary))
 
-  (use-package evil-nerd-commenter
-	:ensure t
-	:commands (evilnc-comment-operator)
-	:init
-	(general-define-key
-	 :states '(normal visual)
-	 "g/" 'evilnc-comment-operator))
+(use-package evil-lion
+  :defer t
+  :ensure t
+  :commands (evil-lion-left evil-lion-right)
+  :init
+  (general-define-key
+   :states '(normal visual)
+   "x<" #'evil-lion-left
+   "x>" #'evil-lion-right)
+  :config
+  (setq evil-lion-left-align-key (kbd "l <")
+		evil-lion-right-align-key (kbd "l >"))
+  (evil-lion-mode))
 
-  ;; As of June 14, evil-commentary is a bit buggy when the whitespace mode is on.
-  ;; (use-package evil-commentary
-  ;; 	:ensure t
-  ;; 	:commands (evil-commentary)
-  ;; 	:init
-  ;; 	(general-define-key
-  ;; 	 :states '(normal visual)
-  ;; 	 "g/" 'evil-commentary))
+(use-package drag-stuff
+  :ensure t
+  :commands (drag-stuff-up drag-stuff-down drag-stuff-left drag-stuff-right)
+  :init
+  (general-define-key
+   :states '(normal visual)
+   "M-c" 'drag-stuff-up
+   "M-t" 'drag-stuff-down
+   "M-n" 'drag-stuff-right
+   "M-h" 'drag-stuff-left))
 
-  (use-package evil-lion
-	:defer t
-	:ensure t
-	:commands (evil-lion-left evil-lion-right)
-	:init
-	(general-define-key
-	 :states '(normal visual)
-	 "x<" #'evil-lion-left
-	 "x>" #'evil-lion-right)
-	:config
-	(setq evil-lion-left-align-key (kbd "l <")
-		  evil-lion-right-align-key (kbd "l >"))
-	(evil-lion-mode))
+(use-package evil-replace-with-register
+  :ensure t
+  :commands 'evil-replace-with-register
+  :init
+  (general-define-key
+   :states '(normal visual)
+   ;; Remove the Replace state with something more useful
+   "r" 'evil-replace-with-register))
 
-  (use-package drag-stuff
-	:ensure t
-	:commands (drag-stuff-up drag-stuff-down drag-stuff-left drag-stuff-right)
-	:init
-	(general-define-key
-	 :states '(normal visual)
-	 "M-c" 'drag-stuff-up
-	 "M-t" 'drag-stuff-down
-	 "M-n" 'drag-stuff-right
-	 "M-h" 'drag-stuff-left))
+(use-package evil-numbers
+  :ensure t
+  :commands (evil-numbers/dec-at-pt evil-numbers/inc-at-pt)
+  :init
+  (general-define-key
+   :states '(normal visual)
+   "g-" 'evil-numbers/dec-at-pt
+   "g+" 'evil-numbers/inc-at-pt))
 
-  (use-package evil-replace-with-register
-	:ensure t
-	:commands 'evil-replace-with-register
-	:init
-	(general-define-key
-	 :states '(normal visual)
-	 ;; Remove the Replace state with something more useful
-	 "r" 'evil-replace-with-register))
+;; (use-package aggressive-indent
+;;   :hook ((prog-mode . aggressive-indent-mode)
+;; 		 (text-mode . aggressive-indent-mode)))
 
-  (use-package evil-numbers
-  	:ensure t
-    :commands (evil-numbers/dec-at-pt evil-numbers/inc-at-pt)
-    :init
-    (general-define-key
-     :states '(normal visual)
-     "g-" 'evil-numbers/dec-at-pt
-     "g+" 'evil-numbers/inc-at-pt))
-
-  ;; (use-package aggressive-indent
-  ;;   :hook ((prog-mode . aggressive-indent-mode)
-  ;; 		 (text-mode . aggressive-indent-mode)))
-
-  ;; (use-package fold-this
-  ;;   :commands 'fold-active-region-all
-  ;;   :init
-  ;;   (general-define-key
-  ;;    :states 'visual
-  ;;    "." #'fold-active-region-all))
+;; (use-package fold-this
+;;   :commands 'fold-active-region-all
+;;   :init
+;;   (general-define-key
+;;    :states 'visual
+;;    "." #'fold-active-region-all))
 
