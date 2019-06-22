a55586a69363b5499a670773dd0e5aa8442da2d7
Dr. Yad Tahir <yad@ieee.org>
2019-06-22 09:12:13 +0300

    Re-arrange the settings in general.el to group them logically

--- emacs.d/modules/ui/general.el
+++ emacs.d/modules/ui/general.el
@@ -19,14 +19,17 @@
 
 (setq inhibit-startup-message t
 	  inhibit-startup-echo-area-message t
-	  user-mail-address "me@yad.email"
-	  ;; debug-on-error t
-	  bidi-display-reordering nil ; tiny performance boost
-	  blink-matching-paren nil    ; don't blink--too distracting
-	  cursor-in-non-selected-windows nil  ; hide cursors in other windows
+	  user-mail-address "yad@ieee.org"
+
+	  ;; Windows/Frames
 	  frame-inhibit-implied-resize t
-	  confirm-kill-processes nil ;; Kill all pprocesses without confirmation
 	  highlight-nonselected-windows nil
+	  max-mini-window-height 0.3
+	  resize-mini-windows 'fit  ; Minibuffer resizing
+	  use-dialog-box nil ; always avoid GUI
+	  auto-window-vscroll nil
+
+	  ;; Files
 	  delete-old-versions -1
 	  make-backup-files t
 	  backup-directory-alist `((".*" . ,temporary-file-directory))
@@ -34,38 +37,45 @@
 	  auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
 	  coding-system-for-read 'utf-8
 	  coding-system-for-write 'utf-8
-	  sentence-end-double-space nil
-	  max-lisp-eval-depth 10000
-	  gc-cons-threshold 20000000
 	  large-file-warning-threshold 100000000 ;; 100MB!
-	  ;; image-animate-loop t
-	  indicate-buffer-boundaries nil
-	  indicate-empty-lines nil
-	  max-mini-window-height 0.3
+
+	  ;; Mouse
 	  mode-line-default-help-echo nil ; disable mode-line mouseovers
+	  cursor-in-non-selected-windows nil  ; hide cursors in other windows
 	  mouse-yank-at-point nil  ; disable mouse craziness
-	  resize-mini-windows 'fit  ; Minibuffer resizing
-	  show-help-function nil          ; hide :help-echo text
-	  uniquify-buffer-name-style 'forward
-	  uniquify-strip-common-suffix nil
-	  use-dialog-box nil              ; always avoid GUI
 	  visible-cursor nil
 	  x-stretch-cursor nil
+	  ;; Smooth scrolling please!
+	  ;; mouse-wheel-scroll-amount '(1 ((shift . 1)))
+	  mouse-wheel-progressive-speed nil ;; no scrolling acceleration
+	  mouse-wheel-follow-mouse t ;; scroll window under mouse
+
+	  ;; Keyword
+	  scroll-step 1
+	  scroll-conservatively 10000
+	  scroll-margin 0
+
+	  ;; Performance
 	  ;; Defer jit font locking slightly to [try to] improve Emacs performance
 	  jit-lock-defer-time nil
 	  jit-lock-stealth-nice 0.1
 	  jit-lock-stealth-time 0.2
 	  jit-lock-stealth-verbose nil
+	  max-lisp-eval-depth 10000
+	  bidi-display-reordering nil ; tiny performance boost
+	  gc-cons-threshold 20000000
+	  uniquify-buffer-name-style 'forward
+	  uniquify-strip-common-suffix nil
 
-	  ;; Smooth scrolling please!
-	  ;; mouse-wheel-scroll-amount '(1 ((shift . 1)))
-	  mouse-wheel-progressive-speed nil ;; no scrolling acceleration
-	  mouse-wheel-follow-mouse t ;; scroll window under mouse
-	  ;; keyword-base scrolling
-	  scroll-step 1
-	  scroll-conservatively 10000
-	  scroll-margin 0
-	  auto-window-vscroll nil
+	  ;; Other
+	  blink-matching-paren nil    ; don't blink--too distracting
+	  confirm-kill-processes nil ;; Kill all pprocesses without confirmation
+	  sentence-end-double-space nil
+	  ;; debug-on-error t
+	  ;; image-animate-loop t
+	  indicate-buffer-boundaries nil
+	  indicate-empty-lines nil
+	  show-help-function nil          ; hide :help-echo text
 	  ;; Disable multi-line echo messages. They are really annoying when you
 	  ;; jump between lines.
 	  eldoc-echo-area-use-multiline-p nil
@@ -73,13 +83,11 @@
 	  ring-bell-function #'ignore
 	  visible-bell nil)
 
-
 (setq-default fill-column 80
 			  tab-width 4
 			  tooltip-delay 5
 			  indent-tabs-mode t)
 
-(put 'narrow-to-region 'disabled nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (desktop-save-mode 0)
@@ -89,9 +97,3 @@
 ;; (scroll-bar-mode 0)
 ;; (tool-bar-mode -1)
 ;; (menu-bar-mode -1)
-
-(cl-eval-when (compile)
-  (require 'recentf))
-
-(with-eval-after-load 'recentf
-  (setq recentf-max-menu-items 500))
