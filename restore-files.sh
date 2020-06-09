6b70dc44039619070399a83236b173dd1f5fb9b9
Dr. Yad Tahir <yad@ieee.org>
2020-06-09 16:15:33 +0300

    Revert "restore files concurrently"
    Running the restore scripts in parallel starts to cause many problems,
    especially with sudo when the CPU is 100% utilized. As a workaround, the commit
    commit 3315f3a63b3057d231067f72b7e2ffb9cf528070 is reverted as not restoring files
    concurrently is not big of a deal.

--- restore-files.sh
+++ restore-files.sh
@@ -28,34 +28,33 @@ if [ $? -ne 0 ]; then
 	exit $?
 fi
 
-./emacs/restore-linux.sh &
-./dmenu/restore-linux.sh &
-./dwm/restore-linux.sh &
-./firewall/restore-linux.sh &
-./gdrive/restore-linux.sh &
-./git/restore-linux.sh &
-./igpu/restore-linux.sh &
-./logger/restore-linux.sh &
-./kernel/restore-linux.sh &
-./monitor/restore-linux.sh &
-./mpd/restore-linux.sh &
-./network/restore-linux.sh &
-./nvidia/restore-linux.sh &
-# ./pacman/restore-linux.sh &
-./pam/restore-linux.sh &
-./portage/restore-linux.sh &
-./power/restore-linux.sh &
-./pulse/restore-linux.sh &
-./root/restore-linux.sh &
-./scripts/restore-linux.sh &
-./shell/restore-linux.sh &
-./visualizer/restore-linux.sh &
-./urxvt/restore-linux.sh &
-./vm/restore-linux.sh &
-./vpn/restore-linux.sh &
-./x11/restore-linux.sh &
-./zathura/restore-linux.sh &
-./zfs/restore-linux.sh &
+./emacs/restore-linux.sh
+./dmenu/restore-linux.sh
+./dwm/restore-linux.sh
+./firewall/restore-linux.sh
+./gdrive/restore-linux.sh
+./git/restore-linux.sh
+./igpu/restore-linux.sh
+./logger/restore-linux.sh
+./kernel/restore-linux.sh
+./monitor/restore-linux.sh
+./mpd/restore-linux.sh
+./network/restore-linux.sh
+./nvidia/restore-linux.sh
+# ./pacman/restore-linux.sh
+./pam/restore-linux.sh
+./portage/restore-linux.sh
+./power/restore-linux.sh
+./pulse/restore-linux.sh
+./root/restore-linux.sh
+./scripts/restore-linux.sh
+./shell/restore-linux.sh
+./visualizer/restore-linux.sh
+./urxvt/restore-linux.sh
+./vm/restore-linux.sh
+./vpn/restore-linux.sh
+./x11/restore-linux.sh
+./zathura/restore-linux.sh
+./zfs/restore-linux.sh
 
-wait
 echo "Done."
