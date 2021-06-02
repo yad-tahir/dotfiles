13024d72c16b663b9260c1ec5ee7f402b1ee50ea
Dr. Yad Tahir <yad@ieee.org>
2021-06-02 22:32:54 +0300

    dwm: Fix vault's vertical screen space

--- monitor/local/bin/screen-layout/home-vault.sh
+++ monitor/local/bin/screen-layout/home-vault.sh
@@ -21,7 +21,11 @@
 cd `dirname $0`
 . $PWD/util.sh
 
-xrandr --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate left --output DisplayPort-1 --mode 3440x1440 --pos 2360x155 --rotate inverted --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 3840x1600 --pos 2160x1637 --rotate normal
+xrandr --auto
+xrandr --output DisplayPort-0 --mode 3840x2160 --pos 0x0 --rotate left
+xrandr --output DisplayPort-1 --mode 3440x1440 --pos 2360x196 --rotate inverted
+xrandr --output DisplayPort-2 --off
+xrandr --output HDMI-A-0 --primary --mode 3840x1600 --pos 2160x1637 --rotate normal
 
 # dispwin -d 1 $HOME/.config/icc-profiles/U3818DW#2-2018-10-20-2347.icc
 # dispwin -d 3 $HOME/.config/icc-profiles/U2718Q#3-2018-10-21-0034.icc
@@ -42,8 +46,6 @@ dwmc setmfact 0.5
 urxvtc -hold -name log -e /bin/journalctl -fn 200 &
 sleep 0.1
 urxvtc -hold -name htop -e /usr/bin/htop &
-sleep 0.1
-urxvtc -hold -name sensors -e /sbin/zpool iostat 1 -v &
 sleep 0.1
 dwmc focusmon 1
 
