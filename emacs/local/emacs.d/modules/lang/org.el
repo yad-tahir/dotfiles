133c4df633ec716d0d941b2031c4d688af1e8fd2
Dr. Yad Tahir <yad@ieee.org>
2026-01-11 10:56:36 +0300

    emacs: Fix whitespace

--- emacs/local/emacs.d/modules/lang/org.el
+++ emacs/local/emacs.d/modules/lang/org.el
@@ -16,6 +16,7 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 ;; 02110-1301, USA.
+
 (defvar do--org-latex-preview t
   "Locally utilized to toggle latex preview settings")
 
