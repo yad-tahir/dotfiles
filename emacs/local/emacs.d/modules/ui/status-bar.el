70a2b89a699d6890d3e288398eca8ccd3f891104
Dr. Yad Tahir <yad@ieee.org>
2026-01-25 00:16:14 +0300

    emacs: Fix flycheck indicator and compiler warnings in status bar

--- emacs/local/emacs.d/modules/ui/status-bar.el
+++ emacs/local/emacs.d/modules/ui/status-bar.el
@@ -20,20 +20,6 @@
 ;;; Code:
 
 ;; Make the color of the mode line dynamic
-(with-eval-after-load 'chocolate-theme
-  (defun do--status-bar-change-mode-line-color()
-    (let ((color (cond (buffer-read-only
-                        `(,chocolate-theme-bg . ,chocolate-theme-element+4))
-                       ((buffer-modified-p)
-                        `(,chocolate-theme-bg . ,chocolate-theme-highlight))
-                       (t
-                        `(,chocolate-theme-bg . ,chocolate-theme-white)))))
-      (set-face-background 'mode-line (car color))
-      (set-face-foreground 'mode-line (cdr color))))
-  (add-hook 'post-command-hook 'do--status-bar-change-mode-line-color)
-  (add-hook 'windmove-do-window-select 'do--status-bar-change-mode-line-color)
-  (add-hook 'find-file-hook 'do--status-bar-change-mode-line-color))
-
 (use-package evil-anzu
   :ensure t
   :after evil
@@ -43,7 +29,13 @@
 (use-package telephone-line
   :ensure t
   :demand t
-  :init (declare-function telephone-line-mode nil)
+  :defines (chocolate-theme-bg
+            chocolate-theme-white chocolate-theme-highlight
+            chocolate-theme-element+4 flycheck-last-status-change
+            flycheck-current-errors)
+  :functions (telephone-line-mode
+              anzu--update-mode-line flycheck-count-errors
+              flycheck-list-errors)
   :config
   (setq telephone-line-faces
         '((evil . telephone-line-modal-face)
@@ -54,22 +46,16 @@
   (defun do--status-bar-anzu-segment ()
     "A telephone line segment returns `Anzu segment."
 
-    ;; will be called only once.
-    (eval-and-compile
-      (require 'anzu)
-      (setq anzu-cons-mode-line-p nil))
-    ;; The lambda function below will be called on updates
-    (lambda (face)
-      (ignore face)
+    (require 'anzu)
+    (setq anzu-cons-mode-line-p nil)
+    (lambda (_)
       (when anzu--state
-        (telephone-line-raw (concat
-                             (anzu--update-mode-line)
-                             " " )))))
+        (telephone-line-raw (concat (anzu--update-mode-line) " " )))))
 
   (defun do--status-bar-major-mode-segment ()
     "A telephone line segment returns a single space."
 
-    (lambda (face) (ignore face)
+    (lambda (_)
       (replace-regexp-in-string "-mode$"
                                 ""
                                 (symbol-name major-mode))))
@@ -77,13 +63,12 @@
   (defun do--status-bar-space-segment ()
     "A telephone line segment returns a single space."
 
-    (lambda (face) (ignore face) " "))
+    (lambda (_) " "))
 
   (defun do--status-bar-macro-segment ()
     "A telephone line segment for macro recording"
 
-    (lambda (face)
-      (ignore face)
+    (lambda (_)
       (if defining-kbd-macro
           (if evil-this-macro
               (telephone-line-raw
@@ -93,8 +78,7 @@
   (defun do--status-bar-register-segment ()
     "A telephone line segment for active register"
 
-    (lambda (face)
-      (ignore face)
+    (lambda (_)
       (if evil-this-register
           (telephone-line-raw
            (format " REG:%s" (char-to-string evil-this-register)) nil)
@@ -104,15 +88,13 @@
     "A telephone line segment returns the total number of characters in the current
 buffer."
 
-    (lambda (face)
-      (ignore face)
+    (lambda (_)
       (telephone-line-raw (format " %s " (point-max)) nil)))
 
   (defun do--status-bar-mode-line-segment ()
     "A telephone line segment returns the mode line of the current buffer."
 
-    (lambda (face)
-      (ignore face)
+    (lambda (_)
       (telephone-line-raw (concat
                            ;; Buffer name
                            (telephone-line-raw mode-line-buffer-identification t)
@@ -124,10 +106,46 @@ buffer."
                                (setq result (concat result "+")))
                              result)))))
 
+  (telephone-line-defsegment do--status-bar-flycheck-segment ()
+    "Displays current checker state."
+    (when (bound-and-true-p flycheck-mode)
+      (ignore face)
+      (let* ((text (pcase flycheck-last-status-change
+                     ('finished (if flycheck-current-errors
+                                    (let-alist (flycheck-count-errors flycheck-current-errors)
+                                      (let ((error (or .error 0))
+                                            (warning (or .warning 0))
+                                            (info (or .info 0)))
+                                        ;; Customize this format string to remove "Problems:"
+                                        (propertize
+                                         (format " ✘ %s ‼ %s i %s" error warning info)
+                                         'face 'telephone-line-error)))
+                                  (propertize " ✔" 'face 'telephone-line-unimportant))) ;; No errors
+                     ('running (if (bound-and-true-p flycheck-process-source)
+                                  (propertize " ⟳" 'face 'telephone-line-projectile)
+                                  (propertize " ⟳" 'face 'telephone-line-projectile)))
+                     ('no-checker  (propertize "-" 'face 'telephone-line-unimportant))
+                     ('not-checked "=")
+                     ('errored     (propertize "!" 'face 'telephone-line-error))
+                     ('interrupted (propertize "." 'face 'telephone-line-error))
+                     ('suspicious  "?"))))
+        (propertize text
+                    'help-echo (pcase flycheck-last-status-change
+                                 ('finished "Display errors found by Flycheck")
+                                 ('running "Running...")
+                                 ('no-checker "No Checker")
+                                 ('not-checked "Not Checked")
+                                 ('errored "Error!")
+                                 ('interrupted "Interrupted")
+                                 ('suspicious "Suspicious?"))
+                    'display '(raise 0.0)
+                    'mouse-face '(:box 1)
+                    'local-map (make-mode-line-mouse-map
+                                'mouse-1 #'flycheck-list-errors)))))
+
   (defun do--status-bar-position-segment ()
     "Position segment imitating vim-airline's appearance."
-    (lambda (face)
-      (ignore face)
+    (lambda (_)
       (concat " %c " ;; column
               ;; vertical scrolling
               (format "%2d" (/ (line-number-at-pos)
@@ -141,10 +159,10 @@ buffer."
                              (accent . (telephone-line-vc-segment
                                         telephone-line-erc-modified-channels-segment
                                         telephone-line-process-segment))
-                             (nil . (do--status-bar-anzu-segment
-                                     do--status-bar-mode-line-segment)))
+                             (nil . (do--status-bar-anzu-segment))
+                             (nil . (do--status-bar-mode-line-segment)))
 
-        telephone-line-rhs '((nil . (telephone-line-flycheck-segment
+        telephone-line-rhs '((nil . (do--status-bar-flycheck-segment
                                      telephone-line-misc-info-segment))
                              (accent . (do--status-bar-major-mode-segment
                                         do--status-bar-space-segment
@@ -159,6 +177,21 @@ buffer."
         telephone-line-secondary-right-separator	'telephone-line-nil
         telephone-line-evil-use-short-tag nil)
 
-  (telephone-line-mode))
+  (telephone-line-mode)
+
+  (with-eval-after-load 'chocolate-theme
+    (progn
+      (defun do--status-bar-change-mode-line-color(&rest _)
+        (let ((color (cond (buffer-read-only
+                            `(,chocolate-theme-bg . ,chocolate-theme-element+4))
+                           ((buffer-modified-p)
+                            `(,chocolate-theme-bg . ,chocolate-theme-highlight))
+                           (t
+                            `(,chocolate-theme-bg . ,chocolate-theme-white)))))
+          (set-face-background 'mode-line (car color))
+          (set-face-foreground 'mode-line (cdr color))))
+      (add-hook 'post-command-hook 'do--status-bar-change-mode-line-color)
+      (add-hook 'windmove-do-window-select 'do--status-bar-change-mode-line-color)
+      (add-hook 'find-file-hook 'do--status-bar-change-mode-line-color))))
 
 (provide 'do-status-bar)
