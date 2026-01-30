b1c1f977b8a6cdeaf05f12d638b585d2805f7f38
Dr. Yad Tahir <yad@ieee.org>
2026-01-30 15:14:52 +0300

    emacs: Fix color gradient names in chocolate theme

--- emacs/local/emacs.d/local-packages/chocolate/chocolate-theme.el
+++ emacs/local/emacs.d/local-packages/chocolate/chocolate-theme.el
@@ -44,11 +44,11 @@
 (defcustom chocolate-theme-highlight "#ffba00"
   "Palette - default highlight"
   :type 'string :group 'chocolate-theme)
-(defcustom chocolate-theme-highlight+1 "#ff77ff"
+(defcustom chocolate-theme-highlight+1 "#ffc07c"
   "Palette - highlight class 1"
   :type 'string :group 'chocolate-theme)
-(defcustom chocolate-theme-highlight+2 "#ffc07c"
-  "Palette - highlight class 3"
+(defcustom chocolate-theme-highlight+2 "#ff77ff"
+  "Palette - highlight class 2"
   :type 'string :group 'chocolate-theme)
 
 (defcustom chocolate-theme-element "#e0d0c1"
@@ -104,19 +104,19 @@
                             :inverse-video nil :stipple nil
                             :inherit nil))))
  `(warning ((t (:background unspecified :foreground ,chocolate-theme-highlight :weight bold))))
- `(bold ((t (:background unspecified :foreground ,chocolate-theme-highlight+2 :weight bold))))
+ `(bold ((t (:background unspecified :foreground ,chocolate-theme-highlight+1 :weight bold))))
  `(success ((t (:foreground ,chocolate-theme-element+7))))
- `(error ((t (:background unspecified :foreground ,chocolate-theme-highlight+1 :weight bold))))
+ `(error ((t (:background unspecified :foreground ,chocolate-theme-highlight+2 :weight bold))))
  `(highlight ((t (:foreground ,chocolate-theme-highlight :underline t))))
  `(region ((t (:background ,chocolate-theme-shadow+1 :foreground ,chocolate-theme-highlight))))
  `(secondary-selection ((t (:background ,chocolate-theme-shadow+1 :foreground ,chocolate-theme-element+4))))
  `(lazy-highlight ((t (:inherit secondary-selection))))
  `(header-line ((t (:inherit lazy-highlight :background ,chocolate-theme-bg))))
- `(link ((t (:foreground ,chocolate-theme-highlight+2 :underline nil))))
+ `(link ((t (:foreground ,chocolate-theme-highlight+1 :underline nil))))
  `(link-visited ((t (:foreground ,chocolate-theme-element+8))))
  `(button ((t (:inherit (link) :underline t))))
  `(match ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight))))
- `(minibuffer-prompt ((t (:foreground ,chocolate-theme-highlight+1))))
+ `(minibuffer-prompt ((t (:foreground ,chocolate-theme-highlight+2))))
  `(escape-glyph ((t (:inherit shadow)))) ;;display non-graphic characters
  `(next-error ((t (:inherit (region)))))
  `(query-replace ((t (:inherit (isearch)))))
@@ -144,28 +144,28 @@
  `(font-lock-variable-name-face ((t (:foreground ,chocolate-theme-white))))
  `(font-lock-constant-face ((t (:foreground ,chocolate-theme-element+7))))
  `(font-lock-keyword-face ((t (:foreground ,chocolate-theme-element+3))))
- `(font-lock-type-face ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(font-lock-type-face ((t (:foreground ,chocolate-theme-highlight+1))))
  `(font-lock-preprocessor-face ((t (:foreground ,chocolate-theme-element+8))))
  `(font-lock-regexp-grouping-backslash ((t (:inherit font-lock-string-face))))
- `(font-lock-regexp-grouping-construct ((t (:foreground ,chocolate-theme-highlight+1))))
- `(font-lock-string-face ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(font-lock-regexp-grouping-construct ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(font-lock-string-face ((t (:foreground ,chocolate-theme-highlight+1))))
  `(font-lock-doc-face ((t (:inherit font-lock-string-face :foreground unspecified))))
  `(show-paren-match ((t (:background ,chocolate-theme-element+5 :foreground ,chocolate-theme-white))))
- `(show-paren-mismatch ((t (:background ,chocolate-theme-highlight+1 :foreground ,chocolate-theme-bg))))
+ `(show-paren-mismatch ((t (:background ,chocolate-theme-highlight+2 :foreground ,chocolate-theme-bg))))
 
  `(hi-blue ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+8))))
  `(hi-blue-b ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+8))))
  `(hi-green ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+2))))
  `(hi-green-b ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+1))))
  `(hi-pink ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+3))))
- `(hi-red-b ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+1))))
+ `(hi-red-b ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+2))))
  `(hi-yellow ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight))))
 
  ;; mode-line
  `(mode-line ((t (:background ,chocolate-theme-bg :foreground ,chocolate-theme-white
                               :box (:line-width (1 . 2) :style flat-button)))))
  `(mode-line-buffer-id ((t (:weight bold))))
- `(mode-line-emphasis ((t (:foreground ,chocolate-theme-highlight+1 :weight bold))))
+ `(mode-line-emphasis ((t (:foreground ,chocolate-theme-highlight+2 :weight bold))))
  `(mode-line-highlight ((t (:inherit (highlight)))))
  `(mode-line-inactive ((t (:background ,chocolate-theme-bg :weight light :inherit shadow))))
 
@@ -241,7 +241,7 @@
  ;; dired-mode
  `(dired-directory ((t (:inherit font-lock-builtin-face :weight bold))))
  `(dired-symlink ((t (:inherit font-lock-constant-face :weight bold))))
- `(dired-broken-symlink ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+1))))
+ `(dired-broken-symlink ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+2))))
  `(dired-header ((t (:foreground ,chocolate-theme-element+3 :weight bold))))
  ;; `(dired-marked ((t (:weight normal :inherit warning))))
 
@@ -277,13 +277,13 @@
  `(org-block ((t (:foreground ,chocolate-theme-white+1))))
  `(org-todo ((t (:inherit font-lock-builtin-face))))
  `(org-done ((t (:foreground ,chocolate-theme-element))))
- `(org-table ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(org-table ((t (:foreground ,chocolate-theme-highlight+1))))
  `(org-priority ((t (:foreground unspecified))))
  `(org-drawer ((t (:foreground ,chocolate-theme-shadow+2 :inherit font-lock-comment-face))))
- `(org-date ((t (:foreground ,chocolate-theme-highlight+2 :inherit font-lock-string-face))))
+ `(org-date ((t (:foreground ,chocolate-theme-highlight+1 :inherit font-lock-string-face))))
  `(org-scheduled-today ((t (:foreground ,chocolate-theme-white))))
  `(org-scheduled ((t (:foreground ,chocolate-theme-white))))
- `(org-scheduled-previously ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(org-scheduled-previously ((t (:foreground ,chocolate-theme-highlight+1))))
  `(org-clock-overlay ((t (:inherit org-column))))
  `(org-column ((t (:background ,chocolate-theme-shadow+1 :foreground ,chocolate-theme-white+2))))
  `(org-column-title ((t (:background ,chocolate-theme-shadow+1 :foreground ,chocolate-theme-highlight))))
@@ -297,7 +297,7 @@
  `(org-habit-clear-future-face ((t (:foreground unspecified :background unspecified :inherit org-habit-clear-face))))
  `(org-habit-ready-face ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-element+2))))
  `(org-habit-ready-future-face ((t (:foreground unspecified :background unspecified :inherit org-habit-ready-face))))
- `(org-habit-overdue-face ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+1))))
+ `(org-habit-overdue-face ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+2))))
  `(org-habit-overdue-future-face ((t (:foreground unspecified :background unspecified :inherit org-habit-overdue-face))))
 
  `(org-agenda-done ((t (:foreground ,chocolate-theme-white+1))))
@@ -313,7 +313,7 @@
  `(org-roam-preview-heading-highlight ((t (:inherit org-dispatcher-highlight :foreground unspecified :background unspecified))))
 
  ;; Evil
- `(evil-ex-substitute-replacement ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+2))))
+ `(evil-ex-substitute-replacement ((t (:foreground ,chocolate-theme-bg :background ,chocolate-theme-highlight+1))))
 
  ;; Magit
  `(magit-section-heading ((t (:background unspecified :foreground ,chocolate-theme-element+4))))
@@ -345,13 +345,13 @@
                                                :inherit mode-line :weight normal))))
  `(telephone-line-evil-insert ((t (:background ,chocolate-theme-element+2 :foreground ,chocolate-theme-bg
                                                :inherit mode-line :weight normal))))
- `(telephone-line-evil-motion ((t (:background ,chocolate-theme-highlight+2 :foreground ,chocolate-theme-bg
+ `(telephone-line-evil-motion ((t (:background ,chocolate-theme-highlight+1 :foreground ,chocolate-theme-bg
                                                :inherit mode-line :weight normal))))
  `(telephone-line-evil-visual ((t (:background ,chocolate-theme-highlight :foreground ,chocolate-theme-bg
                                                :inherit mode-line :weight normal))))
  `(telephone-line-evil-operator ((t (:background ,chocolate-theme-element+6 :foreground ,chocolate-theme-bg
                                                  :inherit mode-line :weight normal))))
- `(telephone-line-evil-emacs ((t (:background ,chocolate-theme-highlight+1 :foreground ,chocolate-theme-bg
+ `(telephone-line-evil-emacs ((t (:background ,chocolate-theme-highlight+2 :foreground ,chocolate-theme-bg
                                               :inherit mode-line :weight normal))))
  `(telephone-line-evil-replace ((t (:background ,chocolate-theme-element+4 :foreground ,chocolate-theme-bg
                                                 :inherit mode-line :weight normal))))
@@ -375,7 +375,7 @@
  `(ivy-minibuffer-match-face-4 ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-element+2 :weight bold))))
  `(ivy-virtual ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-shadow+2))))
  `(ivy-modified-buffer ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-highlight :weight normal))))
- `(ivy-modified-outside-buffer ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-highlight+1 :weight bold))))
+ `(ivy-modified-outside-buffer ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-highlight+2 :weight bold))))
  `(ivy-org ((t (:inherit nil :background unspecified :foreground ,chocolate-theme-white+1 :weight normal))))
  ;; Make ivy identical to some of the faces in dired mode. However,
  ;; we dont want to inherit from dired as this might trigger auto-loading
@@ -402,7 +402,7 @@
  `(term-color-black ((t (:foreground ,chocolate-theme-shadow+2 :background unspecified))))
  `(term-color-red ((t (:foreground ,chocolate-theme-highlight ))))
  `(term-color-green ((t (:foreground ,chocolate-theme-element ))))
- `(term-color-yellow ((t (:foreground ,chocolate-theme-highlight+2))))
+ `(term-color-yellow ((t (:foreground ,chocolate-theme-highlight+1))))
  `(term-color-blue ((t (:foreground ,chocolate-theme-element+4))))
  `(term-color-magenta ((t (:foreground ,chocolate-theme-element+4))))
  `(term-color-cyan ((t (:foreground ,chocolate-theme-element+4))))
@@ -413,8 +413,8 @@
  ;; Company
  `(company-scrollbar-bg ((t (:background ,chocolate-theme-shadow+1))))
  `(company-scrollbar-fg ((t (:background ,chocolate-theme-shadow+2))))
- `(company-preview ((t (:inherit nil :foreground ,chocolate-theme-highlight+1 :background ,chocolate-theme-bg))))
- `(company-preview-common ((t (:inherit nil :foreground ,chocolate-theme-highlight+1))))
+ `(company-preview ((t (:inherit nil :foreground ,chocolate-theme-highlight+2 :background ,chocolate-theme-bg))))
+ `(company-preview-common ((t (:inherit nil :foreground ,chocolate-theme-highlight+2))))
  `(company-preview-search ((t (:inherit nil :background ,chocolate-theme-highlight :foreground ,chocolate-theme-bg))))
  `(company-tooltip ((t (:foreground ,chocolate-theme-white :background ,chocolate-theme-shadow+1))))
  `(company-tooltip-scrollbar-thumb ((t (:foreground ,chocolate-theme-white :background ,chocolate-theme-shadow+2))))
@@ -432,10 +432,10 @@
   (setq highlight-parentheses-colors
         (list chocolate-theme-highlight chocolate-theme-element+4
               chocolate-theme-element+3 chocolate-theme-element+8
-              chocolate-theme-highlight+1 chocolate-theme-element+1
+              chocolate-theme-highlight+2 chocolate-theme-element+1
               chocolate-theme-element+5 chocolate-theme-element+7
               chocolate-theme-highlight chocolate-theme-element+2
-              chocolate-theme-highlight+2 chocolate-theme-element+6
+              chocolate-theme-highlight+1 chocolate-theme-element+6
               chocolate-theme-highlight chocolate-theme-element
               chocolate-theme-element+8 chocolate-theme-element+8)))
 
